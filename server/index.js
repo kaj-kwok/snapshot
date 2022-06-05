0bf5e7b07d5351b98726f6cf6d0ae4f7cad37eab
Ryan Kwok <kaj.kwok@gmail.com>
2022-06-05 15:41:14 -0700

    added bodyparser to server files

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
+import 'dotenv/config';
 import express from "express";
 import morgan from "morgan";
+import mongoose from "mongoose";
+import bodyParser from "body-parser"
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(morgan('combined'))
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 
 app.get("/", (req, res) => {
   res.send("HOLA MUNDO")
 })
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
-})
+const CONNECTION_URL = process.env.CONNECTION_URL
+
+const db = mongoose.connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`)
+    }))
+  .catch(err => console.log(err))
