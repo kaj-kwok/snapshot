a554d4cdc81962da0e365cd4ada28b728bbb6d2c
Ryan Kwok <kaj.kwok@gmail.com>
2022-06-06 01:02:46 -0700

    added react router

--- client/src/Components/layout.js/mainlinks.js
+++ client/src/Components/layout.js/mainlinks.js
@@ -1,38 +1,41 @@
-import { ThemeIcon, UnstyledButton, Group, Text } from '@mantine/core';
+import { ThemeIcon, UnstyledButton, Group, Text, Anchor } from '@mantine/core';
 import { IconHome, IconBrandInstagram, IconCirclePlus, IconSearch } from '@tabler/icons';
+import { Link } from 'react-router-dom';
 
-const MainLink = ({ icon, color, label }) => {
+const MainLink = ({ icon, color, label, href }) => {
   return (
-    <UnstyledButton
-      sx={(theme) => ({
-        display: 'block',
-        width: '100%',
-        padding: theme.spacing.xs,
-        borderRadius: theme.radius.sm,
-        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+    <Anchor component={Link} to={href} >
+      <UnstyledButton
+        sx={(theme) => ({
+          display: 'block',
+          width: '100%',
+          padding: theme.spacing.xs,
+          borderRadius: theme.radius.sm,
+          color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
 
-        '&:hover': {
-          backgroundColor:
-            theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-        },
-      })}
-    >
-      <Group>
-        <ThemeIcon color={color} variant="light">
-          {icon}
-        </ThemeIcon>
+          '&:hover': {
+            backgroundColor:
+              theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+          },
+        })}
+      >
+        <Group>
+          <ThemeIcon color={color} variant="light">
+            {icon}
+          </ThemeIcon>
 
-        <Text size="sm">{label}</Text>
-      </Group>
-    </UnstyledButton>
+          <Text size="sm">{label}</Text>
+        </Group>
+      </UnstyledButton>
+    </Anchor>
   )
 }
 
 const data = [
-  { icon: <IconHome size={16} />, color: 'blue', label: 'Home' },
-  { icon: <IconBrandInstagram size={16} />, color: 'teal', label: 'Posts' },
-  { icon: <IconCirclePlus size={16} />, color: 'violet', label: 'Create new Post' },
-  { icon: <IconSearch size={16} />, color: 'grape', label: 'Search' },
+  { icon: <IconHome size={16} />, color: 'blue', label: 'Home', href: '/' },
+  { icon: <IconBrandInstagram size={16} />, color: 'teal', label: 'Posts', href: '/posts' },
+  { icon: <IconCirclePlus size={16} />, color: 'violet', label: 'Create new Post', href: '/createpost' },
+  { icon: <IconSearch size={16} />, color: 'grape', label: 'Search', href: '/search' },
 ];
 
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CreatePost from './routes/createpost';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="createpost" element={<CreatePost />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
