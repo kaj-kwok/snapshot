b7b1b37d37e48d5b22b287fa7b7848de5a5a3852
Ryan Kwok <kaj.kwok@gmail.com>
2022-06-06 23:31:52 -0700

    added outline for create post

--- client/src/routes/createpost.js
+++ client/src/routes/createpost.js
@@ -1,6 +1,28 @@
+import { Box, TextInput, Group, Button } from '@mantine/core'
+import { useForm } from '@mantine/form';
+
 const CreatePost = () => {
+  const form = useForm({
+    initialValues: {
+      email: '',
+
+    }
+  })
+
   return (
-    <div>create post</div>
+    <Box sx={{ maxWidth: 300 }} mx="auto">
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <TextInput
+          required
+          label="Email"
+          placeholder="your@email.com"
+          {...form.getInputProps('email')}
+        />
+        <Group position="right" mt="md">
+          <Button type="submit">Create Post</Button>
+        </Group>
+      </form>
+    </Box>
   )
 }
 
