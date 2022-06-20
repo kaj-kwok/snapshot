146d616cc1632c1d8986d30a04dc764af41bcaa5
Ryan Kwok <kaj.kwok@gmail.com>
2022-06-19 22:57:16 -0700

    added navigation buttons to detailedpost

--- client/src/Components/detailedpost/DetailedPost.js
+++ client/src/Components/detailedpost/DetailedPost.js
@@ -2,7 +2,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react'
 import { getPost } from '../../actions/posts';
-import { Card, Text, Group, Badge, Box, Container, Loader } from '@mantine/core';
+import { Card, Text, Group, Badge, Box, Container, Loader, Anchor } from '@mantine/core';
+import { IconArrowLeft, IconArrowRight } from '@tabler/icons';
 
 const DetailedPost = () => {
   const { slug } = useParams()
@@ -29,16 +30,43 @@ const DetailedPost = () => {
           height: 'auto',
           alignSelf: 'center',
           overflow: 'hidden',
+          position: 'relative'
         }}>
+          <Anchor
+            style={{
+              position: 'absolute',
+              left: '0',
+              width: '50%',
+              height: '100%',
+              overflow: 'hidden',
+            }}
+          >
+            <IconArrowLeft size={50} color="white" style={{ position: 'absolute', top: '50%' }} />
+          </Anchor>
+
           <img src={post?.uploadedFile
           } alt="profile" object-fit="contain" height="100%" width="100%" />
+          <Anchor
+            style={{
+              position: 'absolute',
+              right: '0',
+              width: '50%',
+              height: '100%',
+              overflow: 'hidden',
+            }}
+          >
+            <span><IconArrowRight size={50} color="white" style={{ position: 'absolute', top: '50%', right: 0 }} />
+            </span>
+          </Anchor>
+
         </Box>
+
         <Group position="apart" style={{ marginBottom: 5, marginTop: 5 }}>
-          <Text weight={500}>{post.title}</Text>
+          <Text weight={500}>{post?.title}</Text>
         </Group>
 
         <Text size="sm" style={{ color: "grey", lineHeight: 1.5 }}>
-          {post.description}
+          {post?.description}
         </Text>
         <Box style={{ paddingTop: '2em' }}>
           {post?.tags?.map(tag => (
